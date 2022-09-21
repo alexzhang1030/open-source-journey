0d884b719c406dd42a1b8ef7da27bc2ee36bc1e2
LoTwT <709937065@qq.com>
2022-09-21 22:16:26 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,13 @@
+# views
+
+Put some intertesting views by Solid.js and UnoCSS
+
+## Thanks
+
+- [Solid.js](https://github.com/solidjs/solid)
+- [UnoCSS](https://github.com/unocss/unocss)
+- [Vite](https://github.com/vitejs/vite)
+
+## LICENSE
+
+MIT, made with 💗.
---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import type { Component } from 'solid-js'
+import { createSignal } from 'solid-js'
+
+const App: Component = () => {
+  const [count, setCount] = createSignal(0)
+  return (
+    <div
+      overflow-x-hidden min-h="100vh" min-w="100vh"
+      bg="#7EA1C4" text="#1B365C"
+      flex="~" justify-center items-center
+    >
+      <div w-100 flex="~ wrap" justify-center items-center>
+        <div
+          w-full flex="~"
+          justify-center text-12
+        >{count()}</div>
+        <button w-full class="btn" onClick={() => setCount(count() + 1) }>increment count</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
